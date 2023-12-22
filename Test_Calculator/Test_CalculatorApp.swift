1e06a94a8b62ec62c9fd70e532cb8500ce80878c
jaeho.5 <ovsoce@gmail.com>
2023-12-23 05:23:55 +0900

    Initial Commit

---
+++ Test_Calculator/Test_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_CalculatorApp.swift
+//  Test_Calculator
+//
+//  Created by jaeho on 12/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Test_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
